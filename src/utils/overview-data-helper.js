76746887af4b54f73b75e352dd8ee6899bdb1bc1
Soham Bhattacharjee <sbhattacharjee@fanatics.com>
2023-04-16 23:30:48 -0700

    helper method

--- src/utils/overview-data-helper.js
+++ src/utils/overview-data-helper.js
@@ -4,7 +4,7 @@ const processAggregates = (serviceResp) => {
     totalOrder = 0,
     totalSales = 0;
   for (const clientId in serviceResp) {
-    const client = serviceResp[clientId]
+    const client = serviceResp[clientId];
     totalAbandonedCart += client.totalAbandonedCart;
     totalCustomers += client.totalCustomers;
     totalOrder += client.totalOrder;
@@ -13,4 +13,30 @@ const processAggregates = (serviceResp) => {
   return { totalAbandonedCart, totalCustomers, totalOrder, totalSales };
 };
 
-export { processAggregates };
+const processSaleDayOfWeek = (serviceResp) => {
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const returnVal = [];
+
+  for (const clientId in serviceResp) {
+    const client = serviceResp[clientId];
+    const obj = client.salesValueByWeekDays;
+    const data = daysOfWeek
+      .map((day) => ({ primary: day, secondary: obj[day]/100 }))
+      .sort(
+        (a, b) => daysOfWeek.indexOf(a.primary) - daysOfWeek.indexOf(b.primary)
+      );
+    returnVal.push({ data, label: clientId });
+  }
+
+  return returnVal;
+};
+
+export { processAggregates, processSaleDayOfWeek };
