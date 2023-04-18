8ecbb4814ffb605d3bd0f8d05cad7ceb91a7efc4
Soham Bhattacharjee <sbhattacharjee@fanatics.com>
2023-04-17 18:32:12 -0700

    checkpoint

--- src/components/dashboard/client.js
+++ src/components/dashboard/client.js
@@ -1,46 +1,49 @@
-import * as React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
+import React, { useEffect, useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { Button } from "@mui/material";
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
+  { field: "name", headerName: "Name", width: 100 },
+  { field: "address", headerName: "Address", width: 230 },
+  { field: "primaryContact", headerName: "Primary Contact", width: 130 },
   {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    field: "link",
+    headerName: "",
+    width: 150,
+    renderCell: (params) => (
+      <Button variant="outlined" onClick={() => generateQRCode(params.row)}>
+        View QR Code
+      </Button>
+    ),
   },
 ];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+const generateQRCode = (client) => {
+  console.log(client);
+};
+
+const getRowId = (client) => {
+  return client.clientId;
+};
 
 export default function Client() {
+  const [clients, setCient] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const resp = await fetch(`/api/client`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const serviceRespJson = await resp.json();
+      setCient(serviceRespJson);
+    };
+    fetchData();
+  }, []);
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-      />
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid rows={clients} columns={columns} getRowId={getRowId} />
     </div>
   );
 }
