e7a5100069a65d5dbf9004b237ae83475d04c9ea
Soham Bhattacharjee <sbhattacharjee@fanatics.com>
2023-04-29 10:46:22 -0700

    checkpoint

--- src/components/dashboard/client.js
+++ src/components/dashboard/client.js
@@ -9,9 +9,21 @@ import {
   DialogContent,
   DialogActions,
   Stack,
+  TextField,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
 } from "@mui/material";
 import ClientQRCode from "./clientQRcode";
 import { useTheme, styled } from "@mui/material/styles";
+import { STATES } from "../../utils/states";
+
+const renderStateOptions = () => {
+  return Object.keys(STATES).map((state) => {
+    return <MenuItem value={state}>{STATES[state]}</MenuItem>;
+  });
+};
 
 const StyledRoot = styled("div")(() => {
   const theme = useTheme();
@@ -88,7 +100,72 @@ export default function Client() {
         <Dialog open={newClientFormDialog} onClose={handleNewClientFormClose}>
           <DialogTitle>Onboard New Client</DialogTitle>
           <DialogContent>
-            <Stack></Stack>
+            <Stack spacing={4}>
+              <TextField
+                id="store-name"
+                name="clientName"
+                label="Store Name"
+                variant="standard"
+              />
+              <TextField
+                id="addressLine1"
+                name="storeAddress1"
+                label="Address (Street Name, Number)"
+                variant="standard"
+              />
+              <TextField
+                id="addressLine2"
+                name="storeAddress2"
+                label="Address Line 2 (Optional)"
+                variant="standard"
+              />
+              <Stack direction="row" spacing={2}>
+                <TextField
+                  id="addressCity"
+                  name="storeCity"
+                  label="City"
+                  variant="standard"
+                />
+                <FormControl sx={{ minWidth: 120, flexGrow: 1 }}>
+                  <InputLabel id="addressState">State</InputLabel>
+                  <Select
+                    labelId="addressState"
+                    id="state"
+                    label="State"
+                    name="state"
+                  >
+                    {renderStateOptions()}
+                  </Select>
+                </FormControl>
+              </Stack>
+              <Stack direction="row" spacing={2}>
+                <TextField
+                  id="addressZip"
+                  name="zip"
+                  label="Zip"
+                  variant="standard"
+                />
+                <FormControl sx={{ minWidth: 120, flexGrow: 1 }} disabled>
+                  <InputLabel id="addressCountry">Country</InputLabel>
+                  <Select
+                    labelId="addressCountry"
+                    id="addressCountry"
+                    value="United States"
+                    label="Country"
+                  >
+                    <MenuItem selected value="United States">
+                      <em>United States</em>
+                    </MenuItem>
+                  </Select>
+                </FormControl>
+              </Stack>
+              <TextField
+                id="primary-contact"
+                name="primaryContact"
+                label="Contact"
+                variant="standard"
+              />
+            </Stack>
           </DialogContent>
           <DialogActions>
             <Button onClick={handleNewClientFormClose}>Cancel</Button>
