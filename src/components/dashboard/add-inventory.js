991c86cb083895511af85229b3aaf9b6df313f64
Soham Bhattacharjee <sbhattacharjee@fanatics.com>
2023-04-29 19:54:16 -0700

    removes debugger

--- src/components/dashboard/add-inventory.js
+++ src/components/dashboard/add-inventory.js
@@ -42,7 +42,6 @@ export default function AddInventory(props) {
       qty: +formElements.qty.value,
       price: +formElements.price.value,
     };
-    debugger;
   };
   useEffect(() => {
     fetchProducts();
